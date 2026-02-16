742c7118f4b8222392562202454ecb73b8593c63
Nikolai Onken <nikolai@nikolaionken.com>
2026-02-16 16:00:37 +0100

    Implement #51: Warp test runs (#5)

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    setupFiles: ['./test/setup.js'],
     include: ['src/**/*.test.js', 'test/**/*.test.js'],
     coverage: {
       provider: 'v8',
